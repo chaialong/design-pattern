2967d66dfab5a5f7e8f4fec500707a5d165ef615
chaialong <chaialong@gmail.com>
2016-11-07 21:35:19 +0800

    Design pattern: Facade pattern

---
+++ src/main/java/com/facade/pattern/Circle.java
@@ -0,0 +1,7 @@
+package com.facade.pattern;
+
+public class Circle implements Shape {
+    public void draw() {
+        System.out.println("Draw a circle");
+    }
+}
---
+++ src/main/java/com/facade/pattern/Rectangle.java
@@ -0,0 +1,7 @@
+package com.facade.pattern;
+
+public class Rectangle implements Shape {
+    public void draw() {
+        System.out.println("Draw rectangle");
+    }
+}
