38c673d8375b2092b64bf81752dc02ba6f9f3dd0
chaialong <chaialong@gmail.com>
2016-11-08 21:12:51 +0800

    Design pattern: Proxy pattern

---
+++ src/main/java/com/proxy/pattern/Main.java
@@ -0,0 +1,10 @@
+package com.proxy.pattern;
+
+public class Main {
+    public static void main(String[] args) {
+        Image image = new ProxyImage("test.jpg");
+        image.display();
+        System.out.println("");
+        image.display();
+    }
+}
---
+++ src/main/java/com/proxy/pattern/RealImage.java
@@ -0,0 +1,18 @@
+package com.proxy.pattern;
+
+public class RealImage implements Image {
+    private String fileName;
+
+    public RealImage(String fileName) {
+        this.fileName = fileName;
+        loadFromDisk(fileName);
+    }
+
+    private void loadFromDisk(String fileName) {
+        System.out.println("Loading " + fileName);
+    }
+
+    public void display() {
+        System.out.println("Displaying " + fileName);
+    }
+}
