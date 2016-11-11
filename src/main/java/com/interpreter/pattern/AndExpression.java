03db5950a627a1eee74fd22de977b553b13d0c7a
chaialong <chaialong@gmail.com>
2016-11-11 22:21:31 +0800

    Design pattern: Interpreter Pattern

---
+++ src/main/java/com/interpreter/pattern/AndExpression.java
@@ -0,0 +1,15 @@
+package com.interpreter.pattern;
+
+public class AndExpression implements Expression {
+    private Expression exp1;
+    private Expression exp2;
+
+    public AndExpression(Expression exp1, Expression exp2) {
+        this.exp1 = exp1;
+        this.exp2 = exp2;
+    }
+
+    public boolean interpret(String context) {
+        return exp1.interpret(context) && exp2.interpret(context);
+    }
+}
---
+++ src/main/java/com/interpreter/pattern/Expression.java
@@ -0,0 +1,5 @@
+package com.interpreter.pattern;
+
+public interface Expression {
+    public boolean interpret(String context);
+}
