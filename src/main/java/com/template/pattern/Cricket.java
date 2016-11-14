77ebebb484dc7e579d18fbbe82442dc67d416cd8
chaialong <chaialong@gmail.com>
2016-11-14 20:53:14 +0800

    Design pattern: Template Pattern

---
+++ src/main/java/com/template/pattern/Cricket.java
@@ -0,0 +1,15 @@
+package com.template.pattern;
+
+public class Cricket extends Game {
+    void initialize() {
+        System.out.println("Cricket initializing...");
+    }
+
+    void startPlay() {
+        System.out.println("Cricket start play...");
+    }
+
+    void endPlay() {
+        System.out.println("Cricket end play...");
+    }
+}
---
+++ src/main/java/com/template/pattern/Football.java
@@ -0,0 +1,15 @@
+package com.template.pattern;
+
+public class Football extends Game {
+    void initialize() {
+        System.out.println("Football initializing...");
+    }
+
+    void startPlay() {
+        System.out.println("Football start to play");
+    }
+
+    void endPlay() {
+        System.out.println("Football end play");
+    }
+}
